ef3d55aef5314085edd4f03188b4479b7b0408b2
emanuelrzolet <emanuelzolet1@gmail.com>
2024-03-11 16:05:18 -0300

    Ajustei o arquivo das habilidades

--- src/scripts/indicador-progresso.js
+++ src/scripts/indicador-progresso.js
@@ -1,21 +1,25 @@
-let progressBar = document.querySelector(".circular-progress");
-let valueContainer = document.querySelector(".value-container");
+const elementosLi = document.querySelectorAll("#habilidades-container li");
 
-let valorOculto = document.querySelector(".valor-oculto");
-let valor = valorOculto.textContent;
+elementosLi.forEach(elementoLi => {
+  const progressBar = elementoLi.querySelector(".circular-progress");
+  const valueContainer = elementoLi.querySelector(".value-container");
+  const valorOculto = elementoLi.querySelector("#valor-oculto");
+  const valor = valorOculto.textContent;
 
-let progressValue = 0;
-let progressEndValue = 80;
-let speed = 15;
+  let progressValue = 0;
+  let progressEndValue = valor;
+  let speed = 15;
 
-let progress = setInterval(() => {
-	progressValue++;
-	valueContainer.textContent = `${progressValue}%`;
-	progressBar.style.background = `conic-gradient(
-    #F85E00 ${progressValue * 3.6}deg,
+  let progress = setInterval(() => {
+    progressValue++;
+    valueContainer.textContent = `${progressValue}%`;
+    progressBar.style.background = `conic-gradient(
+      #F85E00 ${progressValue * 3.6}deg,
       #121212 ${progressValue * 3.6}deg
-  )`;
-	if (progressValue == progressEndValue) {
-		clearInterval(progress);
-	}
-}, speed);
+    )`;
+
+    if (progressValue == progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+});
