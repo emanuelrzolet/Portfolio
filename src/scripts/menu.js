b6a01c5f92435f5cdafc5b850bdac299ce874d10
emanuelrzolet <emanuelzolet1@gmail.com>
2024-03-13 21:16:52 -0300

    Corrigi a barra de navegação

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,11 +6,13 @@ let isMenuAberto = false;
 function abrirMenu() {
 	menu.classList.add("aberto");
 	isMenuAberto = true;
+	menuBotao.classList.add("oculto"); // Adiciona a classe oculto ao botão
 }
 
 function fecharMenu() {
 	menu.classList.remove("aberto");
 	isMenuAberto = false;
+	menuBotao.classList.remove("oculto"); // Remove a classe oculto do botão
 }
 
 menuBotao.addEventListener("click", () => {
