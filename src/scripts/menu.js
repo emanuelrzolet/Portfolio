a00ff454199f10c6342f3ca464d6e38ea254cbcb
emanuelrzolet <emanuelzolet1@gmail.com>
2024-03-21 11:35:58 -0300

    Finalizei o menu responsivo

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,52 +2,60 @@ const menuBotao = document.querySelector(".menu-botao");
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu ul li");
 
+
 let isMenuAberto = false;
 
 function abrirMenu() {
-  menu.classList.add("aberto");
-  isMenuAberto = true;
-  menuBotao.classList.add("oculto"); // Adiciona a classe oculto ao botão
+	menu.classList.add("aberto");
+	isMenuAberto = true;
+	menuBotao.classList.add("oculto"); // Adiciona a classe oculto ao botão
 }
 
 function fecharMenu() {
-  menu.classList.remove("aberto");
-  isMenuAberto = false;
-  menuBotao.classList.remove("oculto"); // Remove a classe oculto do botão
+	menu.classList.remove("aberto");
+	isMenuAberto = false;
+	menuBotao.classList.remove("oculto"); // Remove a classe oculto do botão
 }
 
 menuBotao.addEventListener("click", () => {
-  if (isMenuAberto) {
-    fecharMenu();
-  } else {
-    menuItems.forEach(item => {
-      item.style.display = 'block';
-    });
-    abrirMenu();
-  }
+  menu.style.width = "30vw";
+  menu.style.justifyContent = 'center';
+	if (isMenuAberto) {
+		fecharMenu();
+	} else {
+
+		menuItems.forEach((item) => {
+			item.style.display = "block";
+		});
+		abrirMenu();
+	}
 });
 
 function verificarPosicao() {
-  const posicaoAtual = window.scrollY;
+	const posicaoAtual = window.scrollY;
 
-  if (posicaoAtual === 0) {
-    menuItems.forEach(item => {
-      item.style.display = 'inline-block';
-    });
-    abrirMenu();
-  } else {
-    fecharMenu();
-  }
+	if (posicaoAtual === 0) {
+		menu.style.width = "70vw";
+    menu.style.justifyContent = 'right';
+
+		menuItems.forEach((item) => {
+			item.style.display = "inline-block";
+		});
+		abrirMenu();
+	} else {
+		fecharMenu();
+	}
 }
 
 window.addEventListener("scroll", verificarPosicao);
 
 // Adicionar evento para verificar o tamanho da tela ao carregar a página
 window.addEventListener("load", () => {
-  const larguraTela = window.innerWidth;
-  const limiteMobile = 768; // Defina o limite de largura para considerar como dispositivo móvel
+	const larguraTela = window.innerWidth;
+	const limiteMobile = 768; // Defina o limite de largura para considerar como dispositivo móvel
 
-  if (larguraTela > limiteMobile) { // Verifica se não é um dispositivo móvel
-    abrirMenu(); // Abre o menu se não for um dispositivo móvel
-  }
+	if (larguraTela > limiteMobile) {
+		// Verifica se não é um dispositivo móvel
+		abrirMenu(); // Abre o menu se não for um dispositivo móvel
+	}
 });
